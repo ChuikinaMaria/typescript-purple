33360bfff00365fb39944d7b10bd0090c3b13535
Maria Chuikina <maria.chuikina@gmail.com>
2022-09-20 13:10:29 +0200

    Optional(?)

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 "use strict";
 const user = {
-    name: 'MAsha',
-    age: 37
+    login: "á@a.ru",
+    password: '1'
 };
+function multiply(first, second) {
+    if (!second) {
+        return first * first;
+    }
+    else
+        return first * second;
+}
+function multiply_5(first, second = 5) {
+    return first * second;
+}
+console.log(multiply(3));
+console.log(multiply_5(3));
--- app.ts
+++ app.ts
@@ -1,21 +1,39 @@
 interface IUser {
-    name: string
+    login: string;
+    password?: string; // ? - пароль опционален
 }
-
-interface IUser {  // классы имплементируют интерфейсы
-    age: number    // в большинстве случаев интерфейсы
-}
+ 
 
 const user: IUser = {
-    name: 'MAsha',
-    age: 37
+    login: "á@a.ru",
+    password: '1'
 }
 
-type ID = string | number; // type  всегда с несколькими типами
-
-interface IID {
-    ID: string | number;
+function multiply(first: number, second?: number): number {
+    if (!second) {
+        return first*first;
+    } else
+    return first * second;
 }
 
+function multiply_5(first: number, second: number = 5): number {
+    return first * second;
+}
 
-   
+console.log(multiply(3));
+console.log(multiply_5(3));
+
+interface IUser_PRO {
+    login: string;
+    password?: {
+        type: 'primary' | 'secondary'
+    }
+}
+function testPass(user: IUser_PRO) {
+    const t = user.password?.type; // const t = user.password? user.password.type : undefined;
+    const p = user.password!.type; // ! - точно будет password
+
+}
+function test(param?: string) {
+    const t = param?? multiply(5); // если нет param, то присваивается значение вычисления функции
+}
