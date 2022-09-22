70036db569f8752568c73be8432111822aaaa2f1
Maria Chuikina <maria.chuikina@gmail.com>
2022-09-22 14:03:16 +0200

    Type Cast - toString, parseInt(), ...user

--- app.ts
+++ app.ts
@@ -1,30 +1,37 @@
-const n: null = null;
-const n1: any = null;
-const n2: number = null;
-const n3: string = null;
-const n4: boolean = null;
-const n5: undefined = null;
+let a = 5;
+let b: string = a.toString();
+let e = new String(a).valueOf(); // без valueOF тип String (объект), а не string
+let f: boolean = new Boolean(a).valueOf();
 
-// "strictNullChecks": false in tsconfig.json , - no errors,
-// true - n2, n3, n4, n5 - error
+let c = 'fgh';
+let d: number = +c; // parseInt(c);
 
-interface IUser{
+interface IUser {
     name: string;
+    email: string;
+    login: string;
 }
 
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null; // null - осознанное отсутствие пользователя
-    } else {
-        return {
-            name: 'Masha'
-        } as IUser
+const user = //<IUSer> - не работает с React
+ {
+    name: 'Masha',
+    email: 'Masha@ya.ru',
+    login: 'Masyanya'
+} as IUser;
+
+interface IAdmin {
+    name: string;
+    role: number;
+}
+
+const admin: IAdmin = {
+    ...user, // спред оператор берет все свойства из user и добавляет в admin
+    role: 1
+} // сохранит и другие свойства, хоть и не явно
+
+function userToAdmin(user: IUser): IAdmin {
+    return {
+        name: user.login, // можно присвоить значение другому ключу
+        role: 1
     }
-}
-
-const user = getUser();
-if (user) {
-    const username = user.name;
-}
-// "strictNullChecks": true проверит, что иногда нет значения (первый ретерн) 
-// alt + up - перемещение строки
+} // полученный объект содержит только нужные поля, иначе объект бует шире, чем нужно.
