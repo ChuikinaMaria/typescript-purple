576fdd0edf9a7af85db8d783073f55e3c9762053
Maria Chuikina <maria.chuikina@gmail.com>
2022-09-22 10:14:37 +0200

    Type Never , switch

--- app.ts
+++ app.ts
@@ -1,45 +1,45 @@
-let input: unknown;
-
-input = 3;
-input = ['sd', 'fgh'];
-
-// let res: string = input;   выдает ошибку, потому что unknown != string
-
-let input_any: any;
-input_any = 3;
-let res_any: string = input_any;  // не выдает ошибку, any можно записать в string
-
-function run(i: unknown) {
-    if (typeof i == 'number') {
-        i++
-    } else {
-        i
-    }
-
+function generateError (message: string): never { // никогда не возвращается
+    throw new Error(message);
 }
 
-run(input);
+function dumpError():never {
+    // return "s"; // - ошибка, потоу что never никогда не возвращает
+    while(true) {}
+}
 
-// type assertion - проверка типа
+function recurtion():never {
+    return recurtion();
+}
 
-async function getData() {
-    try {
-        await fetch("");
-    } catch (error) {
-        // console.log(error.message); ошибка, нельзя обратиться к свойству unknown, к any можно было
-        if (error instanceof Error) {
-            console.log(error.message); // явная проверка, почти всегда
-        }
+// const a: never = 123;  // ошибка, не можем присвоить к never
+const a: void = undefined;
+
+type paymentAction = 'refund' | 'checkout' // | 'reject'; // при добавлении третьего варианта
+// проверка на тип never внизу не срабатывает и можно отловить ошибку на компайле, а не на ране
+
+function processAction(action: paymentAction) {
+    switch (action) {
+        case 'refund':
+            //...
+            break;
+        case 'checkout':
+            //...
+            break;
+        /*case 'reject':
+            //...
+            break;*/  // добавлением третьего кейса исправляем ошибку, добавив новое действие
+        default:
+            const _: never = action; // проверка на тип, action после switch - never 
+            throw new Error('fault action')
     }
 }
-async function getDataForce() {
-    try {
-        await fetch("");
-    } catch (error) {
-        const e = error as Error;
-        console.log(e.message); // выдаст ошибку если е != error
-        }
-    }
 
-type U1 = unknown | null; // union type with unknown always unknown
-type I1 = unknown & string; // intersection goes to narrow type, typeof I1 == string
+function isString(x: string | number): boolean{
+    if (typeof x === 'string') {
+        return true;
+    } else if(typeof x === 'number') {
+        return false;
+    }
+    generateError('oops'); // исчерпывающая проверка на undefined
+}
+
