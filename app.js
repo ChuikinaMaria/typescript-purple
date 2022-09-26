1e66f637cf9695b48f0de4ba3d1c83859501fcfe
Maria Chuikina <maria.chuikina@gmail.com>
2022-09-26 13:03:50 +0200

    extends (order of calling constructors and properties)

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 "use strict";
-class User {
-    set login(l) {
-        this._login = 'from set ' + l;
+class Payment {
+    constructor(id) {
+        this.status = 'new';
+        this.id = id;
     }
-    get login() {
-        return this._login;
+    pay() {
+        this.status = 'paid';
     }
 }
-const user = new User();
-user.login = 'myLogin';
-console.log(user._login);
-console.log(user);
-console.log(user.login);
+class PersistedPayment extends Payment {
+    constructor() {
+        const id = Math.random();
+        super(id); // обращение к конструктору исходного класса, которому наследует
+    }
+    save() {
+        // сохраняет в базу
+    }
+    pay(date) {
+        super.pay(); // override mathod
+        if (date) {
+            this.paidAt = date;
+        }
+    }
+}
+class User {
+    constructor() {
+        this.name = 'user';
+        console.log(this.name);
+    }
+}
+class Admin extends User {
+    constructor() {
+        super();
+        this.name = 'admin';
+        console.log(this.name);
+    }
+}
+new Admin();
--- app.ts
+++ app.ts
@@ -33,6 +33,31 @@ class PersistedPayment extends Payment {
         }
 
     }
+}
 
-new Payment().
-new PersistedPayment().
+class User {
+    name: string = 'user';
+
+    constructor() {
+        console.log(this.name);
+    }
+}
+
+class Admin extends User {
+    name: string = 'admin';
+    constructor() {
+        super();
+        console.log(this.name);
+    }
+}
+
+new Admin();
+new Error();
+
+class HttpError extends Error {
+    code: number
+    constructor(message: string, code?: number) {
+        super(message);
+        this.code = code ?? 500; // Равен code, если нет code, то 500;
+    }
+}
