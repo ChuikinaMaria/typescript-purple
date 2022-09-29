cfe2028f6cc2c3d9fb9a9ab178c90b4646857719
Maria Chuikina <maria.chuikina@gmail.com>
2022-09-29 14:50:42 +0200

    Task - Abstract Logger

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 "use strict";
-class Controller {
-    handleWithLogs(req) {
-        console.log('start');
-        this.handle(req);
-        console.log('end');
+class Logger {
+    printDate(date) {
+        this.log(date.toString());
     }
 }
-// new Controller() - error
-class UserController extends Controller {
-    handle(req) {
-        console.log(req);
+class myLogger extends Logger {
+    log(message) {
+        console.log(message);
+    }
+    logWithDate(message) {
+        this.printDate(new Date());
+        this.log(message);
     }
 }
-const c = new UserController(); // нет ошибки
-c.handleWithLogs('request');
+const a = new myLogger();
+a.logWithDate('Ну нифига себе');
+// abstract class Controller {          // абстрактный класс не инстанциируется
+//     abstract handle(req: any): void; // абс метод только в абс классе
+//     handleWithLogs(req: any) {  // в абстрактный констроллер можно обавить конкретный метод
+//         console.log('start');
+//         this.handle(req);
+//         console.log('end');
+//     }
+// }
+// // new Controller() - error
+// class UserController extends Controller{
+//     handle(req: any): void {
+//         console.log(req);        
+//     }
+// }
+// const c = new UserController(); // нет ошибки
+// c.handleWithLogs('request');
--- app.ts
+++ app.ts
@@ -1,20 +1,43 @@
-abstract class Controller {          // абстрактный класс не инстанциируется
-    abstract handle(req: any): void; // абс метод только в абс классе
+abstract class Logger {
+    abstract log(message: string): void;
 
-    handleWithLogs(req: any) {  // в абстрактный констроллер можно обавить конкретный метод
-        console.log('start');
-        this.handle(req);
-        console.log('end');
+    printDate(date: Date) {
+        this.log(date.toString());
     }
 }
 
-// new Controller() - error
-
-class UserController extends Controller{
-    handle(req: any): void {
-        console.log(req);        
+class myLogger extends Logger {
+    log(message: string): void {
+        console.log(message);
+    }
+    logWithDate(message: string) {
+        this.printDate(new Date());
+        this.log(message);
     }
 }
 
-const c = new UserController(); // нет ошибки
-c.handleWithLogs('request');
+const a = new myLogger();
+a.logWithDate('Ну нифига себе')
+
+
+
+// abstract class Controller {          // абстрактный класс не инстанциируется
+//     abstract handle(req: any): void; // абс метод только в абс классе
+
+//     handleWithLogs(req: any) {  // в абстрактный констроллер можно обавить конкретный метод
+//         console.log('start');
+//         this.handle(req);
+//         console.log('end');
+//     }
+// }
+
+// // new Controller() - error
+
+// class UserController extends Controller{
+//     handle(req: any): void {
+//         console.log(req);        
+//     }
+// }
+
+// const c = new UserController(); // нет ошибки
+// c.handleWithLogs('request');
